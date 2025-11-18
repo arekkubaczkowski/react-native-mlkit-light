df8261d87614e106671ae914a1c61bfc4acbe643
Arkadiusz Kubaczkowski <arek.kubaczkowski@gmail.com>
2025-11-18 11:06:45 +0100

    fixes

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['build/**', 'node_modules/**', 'example/**', 'plugin/build/**']
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx', 'plugin/src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off'
+    }
+  }
+];
